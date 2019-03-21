58722f10965975ea13dbd25da59a2e031272fcdd
Misa Corn <dieuhuongngo@gmail.com>
2019-03-21 10:54:18 +0200

    setting up the app

--- App.js
+++ App.js
@@ -1,3 +1,34 @@
-import App from "./components/index";
+import React, { Component } from "react";
+import {
+  View,
+  FlatList,
+  Button,
+  TextInput,
+  TouchableHighlight,
+  StatusBar,
+  KeyboardAvoidingView
+} from "react-native";
+
+import styles from "./styles";
+
+class App extends Component {
+  state = { movieTitle: "", movieList: [] };
+
+  inputChange = text => {
+    this.setState({ movieTitle: text });
+  };
+
+  findMovie = () => {
+    console.log("hehe");
+  };
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <StatusBar translucent={false} barStyle="dark-content" />
+      </View>
+    );
+  }
+}
 
 export default App;
--- components/index.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-import Home from "./screens/Home";
-
-export default () => <Home />;
