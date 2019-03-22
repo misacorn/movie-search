c49b6b301e4e2632a8e864f99a153137d1520c2d
Misa Corn <dieuhuongngo@gmail.com>
2019-03-22 14:23:27 +0200

    working flatList

--- MovieList.js
+++ MovieList.js
@@ -1,13 +1,18 @@
 import React, { Component } from "react";
-import { View, StyleSheet, Alert } from "react-native";
-import { SearchBar, Header } from "react-native-elements";
+import { View, StyleSheet, Alert, FlatList, Text, Image } from "react-native";
+import { SearchBar } from "react-native-elements";
 
 class MovieList extends Component {
   static navigationOptions = {
-    title: 'Search movie'.toUpperCase(),
+    title: "Search movie".toUpperCase()
   };
-  
+
   state = { title: "", list: [] };
+
+  inputChange = title => {
+    this.setState({ title });
+    this.findMovie();
+  };
 
   findMovie = () => {
     const url = `https://api.themoviedb.org/3/search/movie?api_key=bf2e9e1f41e52b73e962e7a4c7d1e37b&query=${
@@ -28,12 +33,22 @@ class MovieList extends Component {
       <View style={styles.container}>
         <SearchBar
           placeholder="Type movie title here..."
-          onChangeText={title => this.setState({ title })}
+          onChangeText={this.inputChange}
           value={this.state.title}
           lightTheme={true}
           round={true}
           platform="ios"
           cancelButtonTitle="Cancel"
+        />
+        <FlatList
+          style={{ marginLeft: "1%" }}
+          data={this.state.list}
+          keyExtractor={item => item.id}
+          renderItem={({ item }) => (
+            <View>
+              <Text>{item.title}</Text>
+            </View>
+          )}
         />
       </View>
     );
@@ -43,9 +58,5 @@ class MovieList extends Component {
 export default MovieList;
 
 const styles = StyleSheet.create({
-  container: {},
-  header: {
-    alignItems: "center",
-    justifyContent: "center"
-  }
+  container: {}
 });
