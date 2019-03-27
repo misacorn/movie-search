79a22fcc330ff0bc3ef9781d9ddcad6bbc9b2706
Misa <dieuhuongngo@gmail.com>
2019-03-27 23:25:20 +0200

    retrieving data

--- screens/MovieDetails/MovieDetails.js
+++ screens/MovieDetails/MovieDetails.js
@@ -1,36 +1,44 @@
 import React, { Component } from "react";
 import { View, Text } from "react-native";
 
-
+import styles from "./styles";
 
 class MovieDetails extends Component {
-
-  state = {
-    data: []
-  }
+  state = { data: [] };
 
   componentDidMount() {
-    const movieId = this.props.navigation.state.params.id
+    const { navigation } = this.props;
+    const movieId = navigation.state.params.id;
     const url = `https://api.themoviedb.org/3/movie/${movieId}?api_key=bf2e9e1f41e52b73e962e7a4c7d1e37b`;
 
     fetch(url)
-      .then(response => {
-        console.log(response)
-        return response.json()})
+      .then(response => response.json())
       .then(responseJson => {
-        this.setState({ data: responseJson});
+        this.setState({ data: responseJson });
       })
       .catch(err => {
         Alert.alert(err);
       });
-
-
   }
 
   render() {
+    const { data } = this.state;
+    console.log(data.release_date);
     return (
-      <View>
-        <Text>{this.state.data.id}</Text>
+      <View style={styles.itemList}>
+        {/* <Image
+          style={styles.poster}
+          source={{
+            uri: "https://image.tmdb.org/t/p/original" + data.poster_path
+          }}
+        /> */}
+        <View style={styles.briefInfo}>
+          <Text style={styles.title}>{data.title}</Text>
+          <Text>{data.release_date}</Text>
+          {/* <Text style={{ color: data.vote_average >= 7 ? "green" : "red" }}>
+            Rating: {data.vote_average}
+          </Text> */}
+        </View>
       </View>
     );
   }
---
+++ screens/MovieDetails/styles.js
@@ -0,0 +1,12 @@
+import { StyleSheet, Dimensions } from "react-native";
+
+const fullWidth = Dimensions.get("window").width;
+
+export default StyleSheet.create({
+  // itemList: {
+  //   flexDirection: "row",
+  //   width: fullWidth,
+  //   marginBottom: "2%",
+  //   marginTop: "2%"
+  // }
+});
