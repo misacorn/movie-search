5a8ced5edcba9f2f5a5f2b195d1446e37211c867
Misa <dieuhuongngo@gmail.com>
2019-05-08 12:29:45 +0300

    heart button logic

--- button/HeartButton.js
+++ button/HeartButton.js
@@ -5,13 +5,18 @@ import { View, TouchableOpacity } from "react-native";
 class HeartButton extends Component {
   state = { addedToFav: false };
   addToFav = () => {
-    console.log(this.state);
+    this.setState({ addedToFav: !this.state.addedToFav });
   };
   render() {
+    const { addedToFav } = this.state;
     return (
       <TouchableOpacity onPress={this.addToFav}>
         <View>
-          <Icon name="heart-o" size={18} color={"red"} />
+          <Icon
+            name={addedToFav ? "heart" : "heart-o"}
+            size={18}
+            color={"red"}
+          />
         </View>
       </TouchableOpacity>
     );
